b6e2d9a80004e4a4ab2b560a997a38bd4822051c
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2026-03-10 12:24:20 -0300

    entrega 1

--- exercise_statistics.py
+++ exercise_statistics.py
@@ -12,3 +12,27 @@ def statistics():
     num2 = 8
     num3 = 23
     num4 = 12
+    
+    promedio = (num1 + num2 + num3 + num4) / 4
+    print(promedio)
+    
+    nums = [num1, num2, num3, num4]
+    
+    maximo = nums[0]
+    
+    for n in nums:
+        if n > maximo:
+            maximo = n
+            
+    print(maximo)
+    
+    minimo = nums[0]
+    for n in nums:
+        if n < minimo:
+            minimo = n
+            
+    print(minimo)
+    
+    rango = maximo - minimo
+    print(rango)    
+    
